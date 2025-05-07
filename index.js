164e5f314e008acb66db281da020e4be87a59d28
SaraToth <saratoth101@gmail.com>
2025-05-08 08:37:24 +0900

    Feat: Configure custom 404 error page
    Reads a local 404.html when a 404 error is encountered, including an error handler for if this content has an error when being read

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ const fs = require('fs');
 http.createServer(function (req, res) {
     const q = url.parse(req.url, true); // whats this
     const filename = "." + q.pathname; // what's going on here
-    fs.readFile('FILENAME', function (err, data) {
+    fs.readFile(filename, function (err, data) {
         if (err) {
-            res.writeHead(404, { 'content-type': 'text/html'});
-            return res.end("how to get my 404 page here?")
+            fs.readFile('./404.html', function (err404, data404) {
+                if (err404) {
+                    res.writeHead(404, { 'content-type': 'text/html'});
+                    return res.end("404 Page Not Found");
+                }
+                res.writeHead(404, { 'content-type': 'text/html'});
+                res.write(data404);
+                res.end();
+            })
+            return;
         }
         res.writeHead(200, { 'content-type': 'text/html'});
         res.write(data);
