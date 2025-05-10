80aca2abe09183da1b95396264d373aa307bda93
SaraToth <saratoth101@gmail.com>
2025-05-10 18:27:25 +0900

    Feat: Simplify author controller with package
    express-async-handler simplifies author controller by handling errors for async function

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,22 +1,17 @@
 const db = require("../db");
+const asyncHandler = require("express-async-handler");
 
-async function getAuthorById(req, res) {
+const getAuthorById = asyncHandler(async (req, res) => {
     const { authorId } = req.params;
 
-    try {
-        const author = await db.getAuthorById(Number(authorId));
+    const author = await db.getAuthorById(Number(authorId));
 
-        if (!author) {
-            res.status(404).send("Author not found");
-            return;
-        }
-    
-        res.send(`Author Name: ${author.name}`);
-    } catch (error) {
-        console.error("Error retrieving author:", error);
-        res.status(500).send("Internal Server Error");
+    if (!author) {
+        res.status(404).send("Author not found");
+        return;
     }
 
-};
+    res.send(`Author Name: ${author.name}`);
+});
 
 module.exports = { getAuthorById };
