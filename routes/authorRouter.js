7f919021bf20f1927d34da7ffdab7abce78c38f2
SaraToth <saratoth101@gmail.com>
2025-05-10 18:13:22 +0900

    Feat: Configure author controller to router

--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -1,11 +1,10 @@
 const { Router } = require("express");
+const { getAuthorById } = require("../controllers/authorController");
 
 const authorRouter = Router();
 
+// Where / will equal /authors/
 authorRouter.get("/", (req, res) => res.send("All authors"));
-authorRouter.get("/:authorId", (req, res) => {
-    const { authorId } = req.params;
-    res.send(`Author ID: ${authorId}`);
-});
+authorRouter.get("/:authorId", getAuthorById);
 
 module.exports = authorRouter;
