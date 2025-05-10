c2cbb3e658dc284296f13035a3ff16b63f3cce88
SaraToth <saratoth101@gmail.com>
2025-05-10 17:32:12 +0900

    Feat: Update index.js with custom routes

--- index.js
+++ index.js
@@ -2,19 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const authorRouter = require("./routes/authorRouter");
+const bookRouter = require("./routes/bookRouter");
+const indexRouter = require("./routes/indexRouter");
 
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-})
-
-app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "about.html"))
-})
-
-app.get("/contact-me", (req, res) => {
-    res.sendFile(path.join(__dirname, "contact-me.html"));
-})
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
+app.use("/", indexRouter);
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, "404.html"));
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,3 +5,5 @@ const indexRouter = Router();
 indexRouter.get("/", (req, res) => res.send("Home"));
 indexRouter.get("/about", (req, res) => res.send("About"))
 indexRouter.get("/contact", (req, res) => res.send("Contact"));
+
+module.exports = indexRouter;
