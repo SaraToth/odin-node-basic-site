1ea8f24c36b735377203219f9b4f3d52c75d4ae3
SaraToth <saratoth101@gmail.com>
2025-05-10 17:25:14 +0900

    Feat: Create a main router

---
+++ routes/indexRouter.js
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+
+const indexRouter = Router();
+
+indexRouter.get("/", (req, res) => res.send("Home"));
+indexRouter.get("/about", (req, res) => res.send("About"))
+indexRouter.get("/contact", (req, res) => res.send("Contact"));
