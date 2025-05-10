5a939bdbcd281e431e13cc87a41786ebac902704
SaraToth <saratoth101@gmail.com>
2025-05-10 18:22:42 +0900

    Feat: Add error handling to author controller

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -3,14 +3,20 @@ const db = require("../db");
 async function getAuthorById(req, res) {
     const { authorId } = req.params;
 
-    const author = await db.getAuthorById(Number(authorId));
+    try {
+        const author = await db.getAuthorById(Number(authorId));
 
-    if (!author) {
-        res.status(404).send("Author not found");
-        return;
+        if (!author) {
+            res.status(404).send("Author not found");
+            return;
+        }
+    
+        res.send(`Author Name: ${author.name}`);
+    } catch (error) {
+        console.error("Error retrieving author:", error);
+        res.status(500).send("Internal Server Error");
     }
 
-    res.send(`Author Name: ${author.name}`);
 };
 
 module.exports = { getAuthorById };
