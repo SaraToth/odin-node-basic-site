1134ccbca0c2f8d41456c6450adeb7abe318f876
SaraToth <saratoth101@gmail.com>
2025-05-10 18:07:42 +0900

    Feat: Create an author database

---
+++ db.js
@@ -0,0 +1,13 @@
+const { get } = require("./routes/authorRouter");
+
+const authors = [
+    { id: 1, name: "Bryan" },
+    { id: 2, name: "Christian" },
+    { id: 3, name: "Jason" },
+];
+
+async function getAuthorById(authorId) {
+    return authors.find(author => author.id === authorId);
+};
+
+module.exportsf = { getAuthorById };
