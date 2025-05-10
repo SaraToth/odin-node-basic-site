483dba98fc29feb79ff9fd4d474968c962d193c6
SaraToth <saratoth101@gmail.com>
2025-05-10 18:42:13 +0900

    Feat: Create custom error code handler fn

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, "404.html"));
 });
 
+// Every thrown error in the application or the previous middleware function 
+// calling `next` with an error as an argument 
+// will eventually go to this middleware function
+app.use((err, req, res, next) => {
+    console.error(err);
+      // We can now specify the `err.statusCode` that exists in our custom error class 
+      // and if it does not exist it's probably an internal server error
+    res.status(err.statusCode || 500).send(err.message);
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
