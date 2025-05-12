4c341bf2d07718e14b38b97a6c71e0667d239584
SaraToth <saratoth101@gmail.com>
2025-05-12 16:47:33 +0900

    Build: Install and configure EJS

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const path = require('path');
+const path = require('node:path');
 const authorRouter = require("./routes/authorRouter");
 const bookRouter = require("./routes/bookRouter");
 const indexRouter = require("./routes/indexRouter");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
